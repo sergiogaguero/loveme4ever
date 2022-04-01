faab114896e9332e33cf8b0fa7365f5a84b1a958
Julio Truzzi <julio.truzzi@gmail.com>
2022-04-01 15:09:40 -0300

    Change relative path to images

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -20876,7 +20876,7 @@ __exportStar(require("./browser-connect"), exports);
 require("error-polyfill");
 
 },{"./key_stores/browser-index":"../node_modules/near-api-js/lib/key_stores/browser-index.js","./common-index":"../node_modules/near-api-js/lib/common-index.js","./browser-connect":"../node_modules/near-api-js/lib/browser-connect.js","error-polyfill":"../node_modules/error-polyfill/index.js"}],"config.js":[function(require,module,exports) {
-var CONTRACT_NAME = undefined || 'loveme4ever.testnet';
+var CONTRACT_NAME = undefined || 'juliotruzzi.testnet';
 
 function getConfig(env) {
   switch (env) {
@@ -21112,7 +21112,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54764" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63037" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
